543a5299a81aa5ab5013540d1e4be8ea524e442e
sowoniii <5btg2wm945@privaterelay.appleid.com>
2026-05-23 12:56:07 +0900

    chore: comment out standalone output option for easier PM2 start

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone",
   poweredByHeader: false,
   reactCompiler: true,
   async headers() {
